c981bc1062973dfb4b5ac309f3f339726b8f8020
Ezequiel Szuchet <szuchetezequiel@gmail.con>
2020-01-29 17:43:21 -0300

    Subo el index original

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const facturasRouter = require('./facturasCRUD/facturasRouter');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+app.use('/facturas', facturasRouter);
+
+app.use('/', (req, res, next) => { res.status(200).json({code: 0, message: "Estás en la página de inicio"}) });
+
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/finanzas";
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        app.listen(port, () => { console.log(`Corriendo en port ${port}`) })
+    })
+    .catch(err => {
+        console.log(err);
+    });
