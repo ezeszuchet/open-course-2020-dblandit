9cdb1187eee9d3be20167fc29556b606109dcedb
Kevin Szuchet <kevinszuchet@hotmail.com>
2020-01-31 19:43:55 -0300

    cambio nombre carpeta temp

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/cursos', cursosRouter);
 app.use('/', (req, res, next) => { res.status(200).json({code: 0, message: "Estás en la página de inicio"}) });
 
 
-const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/finanzas";
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/tpcursos";
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => {
